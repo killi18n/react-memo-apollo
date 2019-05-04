9171b53b1e8552417a2b97ae6c3d5d4613b2a901
killi8n <evals4dead@gmail.com>
2019-05-04 19:01:31 +0900

    added apollo server

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,11 +1,51 @@
 "use strict";
+var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cooked, raw) {
+    if (Object.defineProperty) { Object.defineProperty(cooked, "raw", { value: raw }); } else { cooked.raw = raw; }
+    return cooked;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var apollo_server_express_1 = require("apollo-server-express");
+var memos = [
+    {
+        id: 1,
+        content: 'hello world',
+        writer: 'dongho',
+        createdAt: '2019-01-02',
+    },
+    {
+        id: 2,
+        content: 'my name is dongho!',
+        writer: 'dongho',
+        createdAt: '2019-01-03',
+    },
+    {
+        id: 3,
+        content: 'Good to see ya!',
+        writer: 'dongho',
+        createdAt: '2019-01-04',
+    },
+];
+var typeDefs = apollo_server_express_1.gql(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n    type Query {\n        memos: [Memo]\n        memo(id: ID!): Memo!\n    }\n\n    type Memo {\n        id: ID!\n        content: String!\n        writer: String!\n        createdAt: String!\n    }\n"], ["\n    type Query {\n        memos: [Memo]\n        memo(id: ID!): Memo!\n    }\n\n    type Memo {\n        id: ID!\n        content: String!\n        writer: String!\n        createdAt: String!\n    }\n"])));
+var resolvers = {
+    Query: {
+        memos: function () { return memos; },
+        memo: function (_, _a) {
+            var id = _a.id;
+            var targetMemo = memos.filter(function (memo) { return memo.id === +id; });
+            return targetMemo[0];
+        },
+    },
+};
+var server = new apollo_server_express_1.ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
 var app = express_1.default();
-var router = express_1.default.Router();
-app.listen(4000, function () {
-    console.log('app is running on port', 4000);
+server.applyMiddleware({ app: app });
+// const router = express.Router();
+app.listen({ port: 4000 }, function () {
+    return console.log("\uD83D\uDE80 Server ready at http://localhost:4000" + server.graphqlPath);
 });
+var templateObject_1;
+//# sourceMappingURL=index.js.map
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,62 @@
 import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+
+const memos = [
+    {
+        id: 1,
+        content: 'hello world',
+        writer: 'dongho',
+        createdAt: '2019-01-02',
+    },
+    {
+        id: 2,
+        content: 'my name is dongho!',
+        writer: 'dongho',
+        createdAt: '2019-01-03',
+    },
+    {
+        id: 3,
+        content: 'Good to see ya!',
+        writer: 'dongho',
+        createdAt: '2019-01-04',
+    },
+];
+
+const typeDefs = gql`
+    type Query {
+        memos: [Memo]
+        memo(id: ID!): Memo!
+    }
+
+    type Memo {
+        id: ID!
+        content: String!
+        writer: String!
+        createdAt: String!
+    }
+`;
+
+type FindByIdPayload = {
+    id: number;
+};
+
+const resolvers = {
+    Query: {
+        memos: () => memos,
+        memo: (_: any, { id }: FindByIdPayload) => {
+            const targetMemo = memos.filter(memo => memo.id === +id);
+            return targetMemo[0];
+        },
+    },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
-const router = express.Router();
+server.applyMiddleware({ app });
 
-app.listen(4000, () => {
-    console.log('app is running on port', 4000);
-});
+// const router = express.Router();
+
+app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
